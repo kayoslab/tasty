8f7b4d0715444c0ea8ad0693aaebbbd89ae0e269
Simon Krüger <hallo@cr0ss.org>
2020-08-13 14:43:44 +0200

    Initial Commit

---
+++ tasty/tasty.h
@@ -0,0 +1,19 @@
+//
+//  tasty.h
+//  tasty
+//
+//  Created by cr0ss on 13.08.20.
+//  Copyright © 2020 cr0ss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tasty.
+FOUNDATION_EXPORT double tastyVersionNumber;
+
+//! Project version string for tasty.
+FOUNDATION_EXPORT const unsigned char tastyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tasty/PublicHeader.h>
+
+
